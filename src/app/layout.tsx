eac8923c7d9af46925439a9794381a00f4935a94
shivang <shivangnov@gmail.com>
2025-01-11 12:36:06 +0530

    adding changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/Header";
+import Provider from "./Provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,14 +40,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
       >
         <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             
           >
+            <Provider>
+            <Header/>
             {children}
+            </Provider>
           </ThemeProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,62 @@
-import Image from "next/image";
+"use client";
+import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="w-full h-dvh flex justify-center items-center">
-      Utile
+      <div className="flex justify-center items-center flex-col">
+        <motion.h1
+          initial={{
+            opacity: 0,
+            scale: 0.95,
+            filter: "blur(10px)",
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+            filter: "blur(0px)",
+          }}
+          transition={{ duration: 0.35, delay: 0 }}
+          className="text-4xl sm:text-6xl font-bold"
+        >
+          Utile
+        </motion.h1>
+        <motion.p
+          initial={{
+            opacity: 0,
+            scale: 0.95,
+            filter: "blur(10px)",
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+            filter: "blur(0px)",
+          }}
+          transition={{ duration: 0.35, delay: 0.35 }}
+          className="text-center text-white/50"
+        >
+          Generate stunning images from text using AI models for free
+        </motion.p>
+        <motion.div
+          initial={{
+            opacity: 0,
+            scale: 0.95,
+            filter: "blur(10px)",
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+            filter: "blur(0px)",
+          }}
+          transition={{ duration: 0.35, delay: 0.7 }}
+        >
+          <Link href="/create">
+            <Button className=" mt-3 font-bold p-5">Start Creating</Button>
+          </Link>
+        </motion.div>
+      </div>
     </div>
   );
 }
